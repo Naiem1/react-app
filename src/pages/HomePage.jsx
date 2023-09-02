6529d72b6606bc7ef3f3836737a64c10dcdc1016
Naiem1 <naiem4288@gmail.com>
2023-09-02 22:15:26 +0600

    added header and subheder section on homepage

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,5 +1,13 @@
+import Header from '../components/header/Header';
+
 const HomePage = () => {
-  return <div className="home">home</div>;
+  return (
+    <div className="home">
+      <div className="home-content">
+        <Header />
+      </div>
+    </div>
+  );
 };
 
 export default HomePage;
