2147c6b4b193c6bdc711415ce20f0f0f1ea6b518
Naiem1 <naiem4288@gmail.com>
2023-09-02 21:14:17 +0600

    initialize react project and structure project file

---
+++ .eslintrc.cjs
@@ -0,0 +1,33 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+  },
+  extends: [
+    'airbnb',
+    'airbnb/hooks',
+    'plugin:react/recommended',
+    'plugin:prettier/recommended',
+  ],
+  overrides: [
+    {
+      env: {
+        node: true,
+      },
+      files: ['.eslintrc.{js,cjs}'],
+      parserOptions: {
+        sourceType: 'script',
+      },
+    },
+  ],
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+  },
+  plugins: ['react', 'prettier'],
+  rules: {
+    'no-console': 'off',
+    'react/function-component-definition': 0,
+    'react/react-in-jsx-scope': 0,
+  },
+};
---
+++ src/app/App.jsx
@@ -0,0 +1,25 @@
+import HomePage from '../pages/HomePage';
+
+const App = () => {
+  return (
+    <div>
+      <HomePage />
+    </div>
+  );
+};
+
+export default App;
+
+/* 
+[] Make sure your app is responsive
+[] The web app will have a header and a sub-header
+[] There will be 5 components with each component with an icon roughly the same size as that in the first image. Each component will have a header and a description.You may take the icons from fontawesome
+[] If the user hovers over the component header it will glow with the same color as the text.
+[] Make sure that your code lets addition of new/extra components on the same page as easy as possible.
+[] Place the components in such a way that none of them overlaps any of the vegetables in your background. Keep all of them on the black background.
+
+
+
+
+
+*/
